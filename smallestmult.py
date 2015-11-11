879f4a1178f4771c328b17d66e65c385d4868b27
leocarr <leo_carr@hotmail.com>
2015-11-11 11:48:44 +1100

    Removed magic number for max multiple
    Created variable to store value of the maximum multiple to allow easier
    upgrade to generic algorithm

--- smallestmult.py
+++ smallestmult.py
@@ -1,17 +1,19 @@
 # smallest multiple
 
+maxmult = 20
 found = False
 smallest = 0 
-current = 20
+current = maxmult
 while found == False:
     div = 0
-    for i in range(1,21):
+    for i in range(1,maxmult+1):
         if current%i == 0:
             div += 1
-    if div == 20:
+    if div == maxmult:
         found = True
         smallest = current
     else:
-      current += 20
+        current += maxmult
 
 print(smallest)
+
